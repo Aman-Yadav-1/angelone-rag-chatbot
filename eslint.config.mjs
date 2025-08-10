2ef726d6db461050b2343dfa79d553cd6e479286
Aman Yadav <107786152+Aman-Yadav-1@users.noreply.github.com>
2025-08-10 18:47:03 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-html-link-for-pages": "off"
+    },
+  },
+  {
+    files: ["app/api/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off"
+    }
+  },
+  {
+    files: ["app/ingest/page.tsx", "app/page.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
